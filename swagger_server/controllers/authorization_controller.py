6c04023ad81f1ef5b029bf06339f66bff7a078f6
szoladz <samuelus@op.pl>
2024-01-19 22:56:02 +0100

    Fix authorization

--- swagger_server/controllers/authorization_controller.py
+++ swagger_server/controllers/authorization_controller.py
@@ -1,9 +1,20 @@
-from typing import List
+import jwt
+from jwt import ExpiredSignatureError, DecodeError
+from werkzeug.exceptions import Unauthorized
+from database import cryptography
+
 """
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
+
+
 def check_bearerAuth(token):
-    return {'test_key': 'test_value'}
-
-
+    try:
+        return jwt.decode(token, cryptography.SALT, algorithms=["HS256"])
+    except ExpiredSignatureError:
+        raise Unauthorized
+    except DecodeError:
+        raise Unauthorized
+    except Exception:
+        raise Unauthorized
